272ac48377c446deb3dcfc7e574d97e61db12039
Bianka Adorján <bianka.adorjan@stud.uni-corvinus.hu>
2021-04-24 21:55:04 +0200

    boatcont

---
+++ HajosTeszt/HajosTeszt/wwwroot/BoatController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HajosTeszt.wwwroot
+{
+    public class BoatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            [HttpGet]
+            [Route("questions/{sorszám}")]
+            public ActionResult M2(int sorszám)
+            {
+                HajostesztContext context = new HajostesztContext();
+                var kérdés = (from x in context.Questions
+                              where x.QuestionId == sorszám
+                              select x).FirstOrDefault();
+
+                if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+                return new JsonResult(kérdés);
+            }
+    }
+}
